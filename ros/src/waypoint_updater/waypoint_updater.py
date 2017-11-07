bf4c99a93f1a1d79407faaec3cb4f1a2f3c5678c
Alex Zhong <zhongzq04@gmail.com>
2017-11-07 11:22:45 -0800

    Add helper methods JMT solver and newton solver

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -6,6 +6,8 @@ from styx_msgs.msg import Lane, Waypoint
 
 from std_msgs.msg import Int32
 
+import numpy as np
+
 import math
 
 '''
@@ -87,7 +89,25 @@ class WaypointUpdater(object):
         # If there is a red light in the final waypoints list
         if stopline_index >= self.starting_index_of_final_wps and stopline_index < self.starting_index_of_final_wps + LOOKAHEAD_WPS:
             # Choose a deceleration mode depending on the distance between current position and stopline
-            # Set target velocity starting from the stopline
+            d = self.distance(self.waypoints_to_pub, 0, stopline_index-self.starting_index_of_final_wps)
+            rospy.loginfo("Distance till red light: %f", d)
+            starting_v = self.get_waypoint_velocity(0)
+            rospy.loginfo("Starting velocity: %f", starting_v)
+            avg_decel = starting_v**2/(2.*d)
+            rospy.loginfo("Estimated average deceleration: %f", avg_decel)
+            decel_max = 1
+            decel = min(avg_decel*1.5, decel_max)
+            # Set target velocity on base_waypoints starting from the stopline
+            decel_scale = [.01, .04, .09, .16, .25, .36, .49, .64, .81]
+            decel_sequence = decel*decel_scale
+            # Set velocity at the stop line at 0.
+            self.set_waypoint_velocity(self.base_waypoints, stopline_index, 0.)
+            v1 = 0.
+            wp1 = stopline_index
+            for i, a in enumerate(decel_sequence):
+                wp2 = stopline_index - (i+1)
+                #v2 = math.sqrt(v1**2 + 2*a*)
+            self.base_waypoints
             # Publish final waypoints
             print(stopline_index)
         rospy.loginfo("Adjusted velocities for a red light at index %d", stopline_index)
@@ -110,6 +130,57 @@ class WaypointUpdater(object):
             wp1 = i
         return dist
 
+def JMT(start, end, T):
+    """
+    Calculate jerk minimizing trajectory for start, end, and T
+    """
+    a0, a1, a2 = start[0], start[1], start[2]/2.0
+    c0 = a0 + a1*T + a2*T**2
+    c1 = a1 + 2*a2*T
+    c2 = 2*a2
+
+    A = np.array([
+            [T**3,   T**4,    T**5],
+            [3*T**2, 4*T**3,  5*T**4],
+            [6*T,    12*T**2, 20*T**3],
+        ])
+    B = np.array([
+            end[0] - c0,
+            end[1] - c1,
+            end[2] - c2
+        ])
+
+    a_3_4_5 = np.linalg.solve(A, B)
+    alphas = np.concatenate([np.array([a0, a1, a2]), a_3_4_5])
+    return alphas
+
+def get_fn_s(alphas):
+    def fn_s(t):
+        #a0, a1, a2, a3, a4, a5 = alphas
+        ts = np.array([1, t, t**2, t**3, t**4, t**5])
+        return np.inner(alphas, ts)
+    return fn_s
+
+def get_fn_v(alphas):
+    def fn_v(t):
+        a0, a1, a2, a3, a4, a5 = alphas
+        return a1 + 2*a2*t + 3*a3*t**2 + 4*a4*t**3 + 5*a5*t**4
+    return fn_v
+
+def newton_solve(f, df, s, bound, tolerance=0.0001):
+    x0 = bound/2.0
+    dx = x0
+    while abs(dx) > tolerance:
+        x1 = x0 - (f(x0)-s)/df(x0)
+        dx = x0 - x1
+        x0 = x1
+    ruturn x0
+
+def sample_points():
+    pass
+
+def fn_interp1d():
+    pass
 
 if __name__ == '__main__':
     try:
